0ae7c37510e8eba5bb0570f2128b4f1e70565c54
Eygen <zhanul.88@gmail.com>
2018-04-05 12:57:56 +0600

    Add selection of number pairs by sum

---
+++ NumberSelection/NumberList.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NumberSelection
+{
+  public static class NumberList
+  {    
+    public static IList<int> GenerateList()
+    {
+      IList<int> numbers = new List<int>();
+      var random = new Random();
+      for (var i = 0; i < 10; i++)
+      {
+        numbers.Add(random.Next(50));
+      }
+      return numbers;
+    }
+
+    public static IList<NumberPair> SelectPairs(this IList<int> numbers, int sum)
+    {
+      IList<NumberPair> numberPairs = new List<NumberPair>();
+      IList<int> selectedList = numbers.Where(n => n < sum).ToList();
+      for(var i = 0; i < selectedList.Count - 1; i++)
+      {
+        int firstNumber = selectedList[i];
+        for (var k = i + 1; k < selectedList.Count; k++)
+        {
+          if (firstNumber + selectedList[k] == sum)
+          {
+            NumberPair pair = new NumberPair(firstNumber, selectedList[k]);
+            numberPairs.Add(pair);
+            selectedList.RemoveAt(k);
+            break;
+          }
+        }
+      }
+      return numberPairs;
+    }
+  }
+
+  public struct NumberPair
+  {
+    public int FirstNumber { get; private set; }
+    public int SecondNumber { get; private set; }
+
+    public NumberPair(int firstNumber, int secondNumber)
+    {
+      FirstNumber = firstNumber;
+      SecondNumber = secondNumber;
+    }
+  }
+}
--- NumberSelection/Program.cs
+++ NumberSelection/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace NumberSelection
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      IList<int> list = NumberList.GenerateList();
+      foreach (var item in list)
+      {
+        Console.WriteLine(item);
+      }
+      Console.WriteLine("Введите целое число:");
+      int sum;
+      enterNumber: string sumStr = Console.ReadLine();      
+      if (!Int32.TryParse(sumStr, out sum))
+      {
+        Console.WriteLine("Вы ввели не число! Попробуйте еще раз:");
+        goto enterNumber;
+      }
+      IList<NumberPair> pairs = list.SelectPairs(sum);
+      foreach(var item in pairs)
+      {
+        Console.WriteLine($"({item.FirstNumber}, {item.SecondNumber})");
+      }
+      Console.ReadKey();
     }
+  }
 }
